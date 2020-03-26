c0fe4bce52df067febaa24383fe9a757fa89b47b
S'unya <sunya.dickman@aerian.com>
2020-03-26 22:28:33 +0000

    preparation for css in js lesson

--- src/function.js
+++ src/function.js
@@ -4,3 +4,28 @@
  * @returns {number}
  */
 export const addOne = (n) => n + 1;
+
+export const solution = (s) => {
+    const strArr = s.split("");
+
+    let arr = [];
+    let single = new Set();
+    let tempMax = 0;
+
+    return strArr.reduce((max, current) => {
+        arr.push(current);
+        single.add(current);
+
+        tempMax = single.size;
+
+        if (arr.length !== tempMax) {
+            const repeatCharIndex = arr.indexOf(current);
+
+            arr = arr.slice(repeatCharIndex + 1);
+
+            single = new Set(arr);
+        }
+
+        return Math.max(max, tempMax);
+    }, 0);
+};
--- src/function.test.js
+++ src/function.test.js
@@ -1,15 +1,32 @@
 // @ts-check
-import { addOne } from "./function";
+import { addOne, solution } from "./function";
+import expectExport from "expect";
 
 describe("the addOne function", () => {
     test("adds one to any number that is passed to it", () => {
         // act
         const two = addOne(1);
         const ten = addOne(9);
-        const nan = addOne();
+        // const nan = addOne();
         // assert
         expect(two).toEqual(2);
         expect(ten).toEqual(10);
-        expect(isNaN(nan)).toBeTruthy();
+        // expect(isNaN(nan)).toBeTruthy();
+    });
+
+    test("retuns correct", () => {
+        const a = solution("nndfddf");
+        const b = solution("aabcdefaf");
+        const c = solution(
+            "NqyOMXRjclIesWnrJegZkwmwIZBxkXqDrsmKXKWBHRFqDoWJPAKGshRbAJgiJcdAhTBONxPWBSgdhNBSeOdmyBJtxHNddVFiCdqjcmuMZPgeORKXGFGlSurVNRmyeTvntoLMCNwamAdsBAECFcyhObznJRUgoUgMFJzuaVBNgGzJkTRfjpSPSEyUgfLatFkiAthVZGyygxzFlwoFxdMahfhCEPSaTutbpLlCKncrsrTXSgjUkdxKELDlillFHMTKNhsHdsvMSsugutHhEWCJYzHARaYUWrYHUKCSuBMptpQuWQhBNgXHrQqJzotXSLzQnFmpeJjKQrxhzMMicHEWUVHMxQXFttaWSgYUmvYQKiyePnBOHJGFzZWuSDNtkJtzmjqtaAigNMglOyMcmJdDAJzwsXDyVpZtDYujZhMUIoFfYCpFTpIXVHwAJFzfdwUdEuZkLuZWxoIOfEKLzuVIRcxUCiVlXVLjOpvzIosvizpgROjqWPstsuJogBlWbbDDDalcODicKMTudehbguEHYjpZIoeuqIPPOcRalTSPRPnshoKQeikngHszxnNfOjBrbgJsUvFRIbyZJACEDivbRbdyTflPfNTsFvwzOqIuhyWhLrgLGIXAQSBqYvKcObfIlyFYkcTOhTDfTGTkuVedjflacZFWoAwlzeerClhPTuUGoEfckiEqTYJKnYpEnOWFdIkTwABHEVLGuCnInIgQGEKbQQyJKsHjTIRyFYXaPupXoaPqpqKhvoYxOklTdQZKBMnufUyZZXRutctUwgUzebbQdrEXeythIoftcxDnMWrsdMdBuYvuusvSqRVLxMTexsIuRpyQPRgWMyhoinOpvqmxMiNZsKCTfLdArOuFoedToRpTYixliyThAhoxQiBvhOEeJqNVUSmoCVejizGdLghIBFECbZEHZuYAMtTWoubcCaydOaSyviLELkJCOQAHPrarWlUytURLhTZSrGJtgGvSerfxkUMsTVNoNIOEticUrGkmpqvfOOOwKAfKKcvUWGRukhrSGSpoJxfACqnFugGBcthpYzMrgFjBEHPboFjFzwRnQXkbGWidnHnfzGVpKqwYQntNIaEDcykrTbiYoejaNVDFoQCJFoeaKoovSZvGlWzsKFUgNOJBUzgYZJkfuZUCfRjDRyORZuOkDPcakQhgAJvggUZCiwojubNIhxKPtYfQukaKSHRKYGrIbUisBiJVCbQILweGFhQfRtwwjrxdIRRKpIXQkfopJaraPrTWblcIBWfgVrAbJIWqdCUdqLwehaIXPksjRtQASVpEYYQuXtCEWAOfqRUyjfLufzuVpEvwlwisXDltRdAweWDopnLzIkHxUuHXIDqHwcfaaRZSpcguVcgaRvsxTLwbQHQWPtjwhiKLnbgAKbziRKxESomiyLiNMIoVNrdDMrJNGqMhhuDSSFEHZHdybgfTkNbwUxsFRNeiuEFZzciLXTMvdiyLQyvgbqgAqbdsXBFPpjbRnbQdWeFnDolORaEIfiWwysSOplXjnThmpNliZzGHpEUPAfhabNOPHnbrlqFZsMbZKwiuBlGlpJeOKgyVcrPBHLKAcsvTuxqBgztWKbszGwGEKofNczFlgMfFoJUcPxRyPazpiyjlYWMVfUkKrkFDoMrgcRHSapYQpJVcAqJeynVRmQOPlFsuIMmUvsEDKXKtWxctCTqcrWyierHfjmTChiLJjMnNnpZJrFqAOacXVBZepJLYFkRjJAhbbamavVslizUvDFXdzMluMrSHfHhbriVVQKGfOUErbcqWTKXlKkjAVKWpKIdaHdhGOMbqMRPGLJMkZBwjbToiylLGNceSuEMyBnnniKxBQbvFiQfIjsvvJnutdeJOMIpsFnQliqHuYYdfnnVlMawmsWWEUPFJtxnGcVKRZbBItkAcxmgQEvUNhnLMHPgjTGVUqkKXVWdrvZKralJimwLQvKlJZiesnNnDuHvgoxzcnRYIdvBvbZMxarLddTYkgilgmC"
+        );
+        const d = solution(
+            "RPpWVwvtnnWitdosJKdTynNhaKteXRdymNicJroyaWttYgZDNkovHsBYzALOuiYJdfpwqzQwxcFQUXCJAVBLxbePwMxbrrPgpJZSPVXsDpNdxSVAwcRPUvQrrefrxvHqjIwAvGgjngTYwlPTjWPYeISUsinlzUoaCQOfUVyWkXeOxVMJjAhBMmQgAeMWpjlrFbWhpRDJBLnyCarRRVbIejfcskZhvXQLeStydJaCFwIxWTdGXGOhfnuCvSCOVCcEtoAfwjGZXNMomDWTdyPoblthyGRdzPGMThMrrErNlGlnQcsDVhVsEEppcDgxIKUGYHRjyvKSFfVIWbFlakkdzFQlrKvebAyDxVWLncDqQSBdQwOOplUpUEwNKWMuXVpgHfapQpgXmHsardnuuObpHOTfaRAyBRRZfhEJTSHrgocNBWQQKIyrzCadEQmeVlcVrxLEwDWuMCbjPrWFuVHCOwOobrNduWVhIeXbbyIsodFwEqtLXFWyqeAuCyVOXjIocAxtKoTtNrQCfLeOScKhlxVYkoImWxhRCoSQwECxFJsfSKwNUHQFCYwNnHqGjZDJheRrVkaEsnkNPNztjYoQMIAUoKoAzNcsayxnUqCVxXhLpXnpnQTbDAJWHXizofLOHAVAMJVbUxmxUgQKrTLuOvjocixsFTAZGmkpHRcllQWwlUnANarBkZOyvvRTvLyjnWWaJtwPFdamDDnxkcAVHViHzDGOughExYOOggTNHceOuLEGiaMELjkAitHYHpiLVRHoUauLuugaEaEyYOYKikmYtEjTrikmjlivUxIKz"
+        );
+
+        expectExport(a).toBe(3);
+        expectExport(b).toBe(6);
+        expectExport(c).toBe(26);
+        expectExport(d).toBe(25);
     });
 });
